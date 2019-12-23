b55214f6788452c6c22f354b5380e8a52d52704c
abir444 <mr.abir444@gmail.com>
2019-12-23 17:58:03 +0600

    api issue

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,5 +1,6 @@
 import React, { useState }  from 'react';
 import { DatePicker,InputNumber, Select, Button } from 'antd';
+import './index.css';
 
 export default () =>{
       // defining select option and set
@@ -51,26 +52,59 @@ const onSearch= () =>{
 
 
     return (
-        <div>
+     
+        <div class='main' style={{ paddingTop: 20 }} >
+         <div class='a'><p><strong> Welcome To Ramtoo Airways</strong></p></div> 
             <div>
             <Select
     showSearch
-    style={{ width: 200 }}
-    placeholder="Select a person"
+    style={{ width: 500 }}
+    placeholder="Select a Carrier"
     optionFilterProp="children"
     onChange={onCarrierChange}
     filterOption={(input, option) =>
       option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
     }
-  >
-    <Option value="EY">EY</Option>
-    <Option value="ET">ET</Option>
-    <Option value="QT">QT</Option>
+  > 
+    <Option value="EK">
+    <div style={{ marginRight: 15 }}>
+                    <img
+                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Emirates_logo.svg/1200px-Emirates_logo.svg.png"
+                      height="20px"
+                      width="20px"
+                    />
+                  </div>
+                  <div style={{ fontSize: 18 }}>Emirates</div>
+    </Option>
+
+    <Option value="ET">
+    <div style={{ marginRight: 15 }}>
+                    <img
+                      src="https://discover.film/wp-content/uploads/2018/03/etihad-airlines-short-films.jpg"
+                      height="20px"
+                      width="20px"
+                    />
+                  </div>
+                  <div style={{ fontSize: 18 }}>Eithid</div>
+    </Option>
+    <Option value="QR">
+    <div style={{ marginRight: 15 }}>
+                    <img
+                      src="https://prague.tv/images/customer/logo/Qatar-Airways-Logo_gallery-big.png"
+                      height="20px"
+                      width="20px"
+                    />
+                  </div>
+                  <div style={{ fontSize: 18 }}>Qatar</div>
+    
+    
+    </Option>
   </Select>
             </div>
-            <div><InputNumber onChange={onFlightNoChange} placeholder='ex 245, 311..'/></div>
+            <div><InputNumber onChange={onFlightNoChange} placeholder='ex 245, 311..'  style={{ width: 500 }}/></div>
             <div> <DatePicker onChange={onChangeDate}/> </div>
             <Button
+             style={{  }}
           type="primary"
           icon="search"
           onClick={onSearch}
@@ -78,9 +112,20 @@ const onSearch= () =>{
         >
           Search
         </Button>
-        {JSON.stringify(data)}
+        {data.flighStatuses && (
+          <div class="status">
+          <h1>Search complete!</h1>
+          <h4>{data.flighStatuses[0].status}</h4>
+          </div>
+        )}
         </div>
 
         
     )
-}
+};
+
+// const style = {
+//   main : {
+    
+//   }
+// }
